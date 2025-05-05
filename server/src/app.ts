f25154e063140f28b9cc38f50cdf77bf4ca4e0c8
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-05-05 17:51:15 +0530

    initialized cors and passport packages created user model

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://byte-messenger.vercel.app", // Frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow cookies and Authorization headers
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 export { app };
