8aae373d04f8b2ff5cbae3b94a2149201255ca79
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-05-05 18:02:55 +0530

    Added friendship model

---
+++ server/src/models/friendship.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const friendSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  recipient: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  }, 
+  status: {
+    type: String,
+    enum: ["pending", "accepted"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Friendship = mongoose.model("Friendship", friendSchema);
+export default Friendship;
