4a089494d28f9bc7f8c677980bbfc4cf8ccc0c7f
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-06-03 11:58:07 +0530

    improved nav styling for mobile devices

--- client/src/components/layout-navbar/LayoutNavbar.tsx
+++ client/src/components/layout-navbar/LayoutNavbar.tsx
@@ -14,7 +14,7 @@ const LayoutNavbar = () => {
       } lg:flex-col justify-between items-center px-4 py-2 lg:px-0 lg:py-4`}
     >
       <div>logo</div>
-      <ul className="list-none flex gap-5 lg:flex-col relative h-full lg:h-fit lg:w-full justify-center items-center">
+      <ul className="list-none flex gap-2 sm:gap-5 lg:flex-col relative h-full lg:h-fit lg:w-full justify-center items-center">
         {navListData?.map((item, i) => {
           const isActive = item.path.includes(pathname);
 
@@ -37,7 +37,7 @@ const LayoutNavbar = () => {
               <Link to={item.path}>
                 <div className="relative z-10 flex justify-center items-center w-full h-full ">
                   <item.icon
-                    className={`w-7 ${
+                    className={`w-6 sm:w-7 ${
                       isActive
                         ? "fill-btn-primary"
                         : "fill-light stroke-[0.7px] stroke-black"
