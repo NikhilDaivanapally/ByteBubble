fd8c8b7230a794ea993782f3d18b91375989de92
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-07-08 16:49:17 +0530

    refactor : remove unwanted audio notification files

--- client/src/constants/sound-map.ts
+++ client/src/constants/sound-map.ts
@@ -1,6 +1,6 @@
 export const soundMap: Record<string, string> = {
   messageSend: "/sounds/outgoing-message.mp3",
   messageReceive: "/sounds/incoming-message.mp3",
-  incomingUnreadMessage: "/sounds/notification-sound-349341.mp3",
+  incomingUnreadMessage: "/sounds/unread-message.mp3",
   // others like callAnswered , rejected etc in future
 };
